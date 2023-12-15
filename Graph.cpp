52230fcb13817f5750a817e1ce795316a6112f32
AmgadShrief <100617535+AmgadShrief@users.noreply.github.com>
2023-12-16 00:19:08 +0200

    "Added code for correction"

--- Graph.cpp
+++ Graph.cpp
@@ -1,8 +1,10 @@
+#include <bits/stdc++.h>
 #include "Graph.h"
 
 Graph::Graph() {
     users.resize(2e5 + 5);
     followersOfUser.resize(2e5 + 5);
+    people_that_user_follow.resize(2e5+5);
 }
 void Graph::add_user(int userId, User& user) {
     users[userId] = user;
@@ -10,4 +12,34 @@ void Graph::add_user(int userId, User& user) {
 
 void Graph::add_follower(int userId, int followerId) {
     followersOfUser[userId].push_back(followerId);
+    people_that_user_follow[followerId].push_back(userId);
+}
+
+vector<User> Graph::most_connections() {
+    int mx = -1;
+    vector<User> ret;
+    for(auto x : users){
+        if(people_that_user_follow[x.get_user_id()].size() > mx)
+            mx = people_that_user_follow[x.get_user_id()].size();
+    }
+    for(auto x : users){
+        if(people_that_user_follow[x.get_user_id()].size() == mx)
+            ret.push_back(x);
+    }
+    return ret;
+}
+
+
+vector<User> Graph::most_followers() {
+    int mx = -1;
+    vector<User> ret;
+    for(auto x : users){
+        if(followersOfUser[x.get_user_id()].size() > mx)
+            mx = followersOfUser[x.get_user_id()].size();
+    }
+    for(auto x : users){
+        if(followersOfUser[x.get_user_id()].size() == mx)
+            ret.push_back(x);
+    }
+    return ret;
 }
--- Graph.h
+++ Graph.h
@@ -5,10 +5,15 @@
 class Graph {
     vector<User> users;
     vector<vector<int>> followersOfUser;
+    vector<vector<int>> people_that_user_follow;
 public:
     Graph();
     void add_user(int userId, User& user);
     void add_follower(int userId, int followerId);
+    vector<User> most_followers();
+    vector<User> most_connections();
+    vector<User> mutual_followers();
+    vector<User> user_suggestion();
 };
 
 #endif
