f640e55d8f9ecec973ee7729efaa0209c8aca324
Sameh Ossama Koleid <samehossama@hotmail.com>
2023-12-28 00:50:29 +0200

    Add XML to JSON conversion using strings to xmlParser class

--- xmlParser.h
+++ xmlParser.h
@@ -1,8 +1,10 @@
 #pragma once
 #include <iostream>
+#include <stack>
 
 class xmlParser {
     private:
+        static int getTagCount(std::string &input_string, std::string &tag_name, int start_index);
         static std::string minifyJson(std::string xml_input);
     public:
         xmlParser() = delete;
@@ -10,6 +12,7 @@ class xmlParser {
         static std::string correct(std::string xml_input);
         static std::string minify(std::string xml_input);
         static std::string prettify(std::string xml_input);
+        static std::string toJsonByStrings(std::string &xml_input);
         static std::string toJson(std::string xml_input);
         static std::string compress(std::string input);
         static std::string decompress(std::string compressed_input);
