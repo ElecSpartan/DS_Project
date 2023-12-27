0145d9266ced4a1467492e644df948f0b3f1f795
MazenSaaed12 <80298280+MazenSaaed11@users.noreply.github.com>
2023-12-27 15:38:25 +0200

    "Added code for correction"

--- main.cpp
+++ main.cpp
@@ -1,13 +1,27 @@
 #include "Error_detection_and_correction.h"
+#include<map>
+string encode(const string &ori) {
+    map<string, int> dict;
+    int dictSize = 256;
+    for (int i = 0; i < dictSize; ++i)
+        dict[string(1, char(i))] = i;
+    string res = "";
+    string s;
+    for (char z: ori) {
+        if (dict.count(s + z)) s += z;
+        else {
+            res += to_string(dict[s]);
+            dict[s + z] = dictSize++;
+            s = z;
+        }
+    }
+    if (!s.empty()) res += to_string(dict[s]);
+    return res;
+}
 int main() {
 
-    string file = read_file("E:\\jj.txt");
-    cout << correct_xml(file);
-    vector<pair<int, bool>> v = get_errors();
-
-    cout << '\n';
-    // o >> opening / 1 >> closing
-    for (auto x: v)
-        cout << x.first << " , " << x.second << '\n';
+    string file = read_file("E:\\College\\sample.txt");
+    string s = encode(file);
+    write_to_file("E:\\College\\xx.txt", s);
     return 0;
 }
