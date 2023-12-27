2af6fbc1d26b2dd4949a2582c26922d79fac86c3
Sameh Ossama Koleid <samehossama@hotmail.com>
2023-12-28 00:50:29 +0200

    Add XML to JSON conversion using strings to xmlParser class

--- xmlParser.cpp
+++ xmlParser.cpp
@@ -1,6 +1,48 @@
 #include "xmlParser.h"
 
 
+int xmlParser::getTagCount (std::string &input_string, std::string &tag_name, int start_index) {
+    std::string opening_tag = "<" + tag_name + ">";
+    std::string closing_tag = "</" + tag_name + ">";
+    std::string next_tag;
+    int next_same_opening_tag = 0, next_same_closing_tag = 0;
+    int next_different_opening_tag_start = 0, next_different_opening_tag_end = 0;
+    int count = 1, base = 0;
+    
+    while (1) {
+        next_same_opening_tag = input_string.find(opening_tag, start_index);
+        next_same_closing_tag = input_string.find(closing_tag, start_index);
+
+        if (next_same_opening_tag == -1) {
+            return count;
+        }
+
+        if (next_same_opening_tag < next_same_closing_tag) {
+            base++;
+        } else {
+            base--;
+        }
+
+        if (base == 0) {
+            next_different_opening_tag_start = input_string.find_first_of("<", next_same_closing_tag + closing_tag.length());
+            if (next_different_opening_tag_start == -1) {
+                break;
+            }
+
+            next_different_opening_tag_end = input_string.find_first_of(">", next_different_opening_tag_start);
+            next_tag = input_string.substr(next_different_opening_tag_start, next_different_opening_tag_end - next_different_opening_tag_start + 1);
+            if (next_tag != opening_tag) {
+                break;
+            }
+
+            count++;
+        }
+
+        start_index = std::min(next_same_opening_tag , next_same_closing_tag) + 1;
+    }
+    return count;
+}
+
 std::string xmlParser::minifyJson(std::string json_input) {
     std::string minified_json;
     int i = 0;
@@ -119,6 +161,110 @@ std::string xmlParser::prettify(std::string xml_input) {
     }
 
     return prettified_xml;
+}
+
+std::string xmlParser::toJsonByStrings(std::string &xml_input) {
+    int i = 0, j;
+    std::string json_output = "", indentation_type = "    ";
+    int level = 0;
+    bool close_tag = false;
+    std::stack<std::string> tag_names;
+    std::stack<int> tag_count;
+
+    
+    while(i < xml_input.length()) {
+        if (xml_input[i] == '<') {
+            int check_for_closing_tag = xml_input.find_first_not_of(" \n\r\t", i + 1);
+            int close_tag_index = xml_input.find('>', i + 1);
+            std::string indentation = "";
+
+            if (xml_input[check_for_closing_tag] == '/') {
+                level--;
+                std::string tag = xml_input.substr(check_for_closing_tag + 1, close_tag_index - check_for_closing_tag - 1);
+                for(int j = 0; j < level + tag_names.size(); j++) indentation += indentation_type;
+
+                if (close_tag) {
+                    json_output += '\n';
+                    json_output += indentation;
+                    json_output += indentation_type;
+                    json_output += '}';
+                }
+                
+                if (!tag_names.empty() && (tag + std::to_string(level + 1)) == tag_names.top() && --tag_count.top() == 0) {
+                    tag_names.pop();
+                    tag_count.pop();
+                    json_output += '\n';
+                    json_output += indentation;
+                    json_output += ']';
+                }
+
+                close_tag = true;
+                i = xml_input.find_first_of('<', i + 1); // bypass the closing tag
+                continue;
+            
+            } else {    // it must be an opening tag
+                level++;
+                std::string tag = xml_input.substr(i + 1, close_tag_index - i - 1);
+                for(int j = 0; j < level + tag_names.size(); j++) indentation += indentation_type;
+
+                int count = xmlParser::getTagCount(xml_input, tag, i);
+
+                if (!close_tag) {
+                    json_output += '{';
+                } else {
+                    json_output += ",";
+                }
+                json_output += '\n';
+                json_output += indentation;
+
+                close_tag = false;
+                i = close_tag_index;
+
+                if (!tag_names.empty() && (tag + std::to_string(level)) == tag_names.top()) {
+                    continue;
+                }
+
+                json_output += '"';
+                json_output += tag;
+                json_output += "\": ";
+
+                if (count != 1) {
+                    tag_names.push(tag + std::to_string(level));
+                    tag_count.push(count);
+                    json_output += "[\n";
+                    json_output += indentation;
+                    json_output += indentation_type;
+                }
+
+                continue;
+            }
+        }
+
+        // check if text
+        int first_after_open_tag = xml_input.find_first_not_of(" \n\r\t", i + 1);
+        if (xml_input[i] == '>' && xml_input[first_after_open_tag] != '<') {
+
+            int closing_tag_start_index = xml_input.find_first_of('<', i + 1);
+            int data_end_index = xml_input.find_last_not_of(" \n\r\t", closing_tag_start_index - 1);
+            int first_non_numerical_char_index = xml_input.find_first_not_of("0123456789-. \n", first_after_open_tag);
+
+            if (first_non_numerical_char_index >= closing_tag_start_index) {
+                json_output += xml_input.substr(first_after_open_tag, data_end_index - first_after_open_tag + 1);
+            } else {
+                json_output += '\"';
+                json_output += xml_input.substr(first_after_open_tag, data_end_index - first_after_open_tag + 1);
+                json_output += '\"';
+            }
+
+            i = closing_tag_start_index;
+            continue;
+        }
+
+        i = first_after_open_tag;
+    }
+
+    json_output += "\n}";
+    return json_output;
 }
 
 std::string xmlParser::toJson(std::string xml_input) {
