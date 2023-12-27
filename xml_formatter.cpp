12faa5cf10038a09c88b680606fe0f2b23658ea5
Sameh Ossama Koleid <samehossama@hotmail.com>
2023-12-27 19:47:20 +0200

    Fix a bug in XML formatting code

--- xml_formatter.cpp
+++ xml_formatter.cpp
@@ -1,18 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    ifstream in("sample.xml");
+string read_file (string path) {
+    ifstream in(path);
+
     stringstream buffer;
     buffer << in.rdbuf();
+    in.close();
+
+    return buffer.str();
+}
+
+void output_file (string path, string output_string) {
+    ofstream out(path);
+    out << output_string;
+    out.close();
+}
+
+int main() {
+    string input_string = read_file("sample.xml");
 
     string s = "";
-    int i = 0, j;
-    string input_string = buffer.str();
+    int i = input_string.find('<'), j;
     int indentation_level = -1;
-    bool close_tag = false, text = false;
+    bool close_tag = false, file_start = true;
 
-    
     while(i < input_string.length()) {
 
         if (input_string[i] == '<') {
@@ -26,41 +38,37 @@ int main() {
                 close_tag = false;
             }
 
-            if(i != 0) s += "\n";
+            if(!file_start) s += "\n";
             for(int j = 0; j < indentation_level; j++) s += "    ";
+            file_start = false;
         }
 
         int first_after_open_tag = input_string.find_first_not_of(" \n\r\t", i + 1);
+        if (first_after_open_tag == -1) {
+            s += '>';
+            break;
+        }
+
         if (input_string[i] == '>' && input_string[first_after_open_tag] != '<') {
-            text = true;
             indentation_level++;
+
+            s += ">\n";
+            for(int j = 0; j < indentation_level; j++) s += "    ";
+
+            int closing_tag_start_index = input_string.find_first_of('<', i + 1);
+            int data_end_index = input_string.find_last_not_of(" \n\r\t", closing_tag_start_index - 1);
+            s += input_string.substr(first_after_open_tag, data_end_index - first_after_open_tag + 1);
+            i = closing_tag_start_index;
+            continue;
         }
         
         s += input_string[i];
         i = first_after_open_tag - 1;
 
-        if (text) {
-            text = false;
-            if(first_after_open_tag == -1) break;
-
-            s += "\n";
-            for(int j = 0; j < indentation_level; j++) s += "    ";
-
-            int data_start_index = first_after_open_tag;
-            int closing_tag_start_index = input_string.find_first_of('<', i + 1);
-            int data_end_index = input_string.find_last_not_of(" \n\r\t", closing_tag_start_index - 1);
-            s += input_string.substr(data_start_index, data_end_index - data_start_index + 1);
-            i = closing_tag_start_index - 1;
-        }
-
         i++;
     }
 
-    // cout << s;
-
-    ofstream out("output_pretty.xml");
-    out << s;
-    out.close();
+    output_file("output_pretty.xml", s);
 
     return 0;
 }
