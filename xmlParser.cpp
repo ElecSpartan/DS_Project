18b00d06db7bb4af4e48a418cdf277f9d9624e11
Sameh Ossama Koleid <samehossama@hotmail.com>
2023-12-28 00:05:40 +0200

    Add XML prettify to xmlParser class

--- xmlParser.cpp
+++ xmlParser.cpp
@@ -56,7 +56,53 @@ std::string xmlParser::minify(std::string xml_input) {
 }
 
 std::string xmlParser::prettify(std::string xml_input) {
-    return std::string();
+    std::string prettified_xml = "<";
+    int i = xml_input.find('<') + 1, j;
+    int indentation_level = 0;
+    bool close_tag = false;
+
+    while(i < xml_input.length()) {
+
+        if (xml_input[i] == '<') {
+            int check_for_closing_tag = xml_input.find_first_not_of(" \n\r\t", i + 1);
+            
+            if (xml_input[check_for_closing_tag] == '/') {
+                close_tag = true;
+                indentation_level--;
+            } else {
+                if (!close_tag) indentation_level++;
+                close_tag = false;
+            }
+
+            prettified_xml += "\n";
+            for(int j = 0; j < indentation_level; j++) prettified_xml += "    ";
+        }
+
+        int first_after_open_tag = xml_input.find_first_not_of(" \n\r\t", i + 1);
+        if (first_after_open_tag == -1) {
+            prettified_xml += '>';
+            break;
+        }
+
+        if (xml_input[i] == '>' && xml_input[first_after_open_tag] != '<') {
+            indentation_level++;
+
+            prettified_xml += ">\n";
+            for(int j = 0; j < indentation_level; j++) prettified_xml += "    ";
+
+            int closing_tag_start_index = xml_input.find_first_of('<', i + 1);
+            int data_end_index = xml_input.find_last_not_of(" \n\r\t", closing_tag_start_index - 1);
+            prettified_xml += xml_input.substr(first_after_open_tag, data_end_index - first_after_open_tag + 1);
+            i = closing_tag_start_index;
+            continue;
+        }
+        
+        prettified_xml += xml_input[i];
+        i = first_after_open_tag - 1;
+        i++;
+    }
+
+    return prettified_xml;
 }
 
 std::string xmlParser::toJson(std::string xml_input) {
