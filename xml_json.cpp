e424d7766debba05c79ea2e00565d941cc181c20
SamehOssama <43967303+SamehOssama@users.noreply.github.com>
2023-12-28 15:01:48 +0200

    Revert "Expand XML to JSON conversion to support array of tags"
    This reverts commit 301d311a0c39f678013161585c338a775950b997.

--- xml_json.cpp
+++ xml_json.cpp
@@ -1,38 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct mypair {
-    string first;
-    int second;
-
-    mypair(){
-        this->first = "";
-        this->second = 0;
-    }
-
-    mypair(string first, int second){
-        this->first = first;
-        this->second = second;
-    }
-        
-};
-
-stack<mypair*> tags;
-
-int get_tag_count(string &input_string, string &tag_name, int start_index, int end_index) {
-    string tag = "<" + tag_name + ">";
-    bool flag = true;
-    int count = 0;
-    
-    while(start_index < end_index && start_index != -1) {
-        count++;
-        int check_next_closing_tag = input_string.find_first_not_of(" \n\r\t", start_index + tag.length());
-        if (input_string[check_next_closing_tag] !=  '<') return 1;
-        start_index = input_string.find(tag, start_index + tag.length());
-    }
-    return count;
-}
-
 int main() {
     ifstream in("sample.xml");
     stringstream buffer;
@@ -40,10 +8,9 @@ int main() {
 
     string s = "";
     int i = 0, j;
-    string input_string = buffer.str(), tag;
+    string input_string = buffer.str();
     int indentation_level = 0;
-    bool close_tag = false, text = false, flag = true;
-    int count;
+    bool close_tag = false, text = false;
 
     
     while(i < input_string.length()) {
@@ -59,34 +26,12 @@ int main() {
                     s += '}';
                 }
 
-                if(!close_tag) { 
-                    indentation_level--;
-                }
-                
-                if (close_tag && tags.top()->second == 1 && tags.size() != 1) {
-                    indentation_level--;
-                    s += '\n';
-                    for(int j = 0; j < indentation_level; j++) s += "    ";
-                    s += ']';
-                }
-
-                if (--tags.top()->second == 0) {
-                    tags.pop();
-                }
+                if(!close_tag) indentation_level--;
 
                 close_tag = true;
                 i = input_string.find_first_of('<', i + 1) - 1; // bypass the closing tag
             
             } else {    // it must be an opening tag
-                int close_tag_index = input_string.find('>', i + 1);
-                tag = input_string.substr(i + 1, close_tag_index - i - 1);
-                int parent_close_tag_index = INT_MAX;
-                if(!tags.empty()) {
-                    parent_close_tag_index = input_string.find("</" + tags.top()->first + ">", i);
-                }
-
-                count = get_tag_count(input_string, tag, close_tag_index, parent_close_tag_index);
-
                 close_tag = false;
 
                 if (!text) {
@@ -100,13 +45,8 @@ int main() {
 
                 s += '\n';
                 for(int j = 0; j < indentation_level; j++) s += "    ";
-
-                if (tags.empty() || tag != tags.top()->first) {
-                    s += '"';
-                    s += tag;
-                }
-
-                i = close_tag_index - 1;
+                // if(!close_tag) do the code below
+                s += '"';
             }
             
             i++;
@@ -121,27 +61,8 @@ int main() {
         }
 
 
-        if (input_string[i] == '>') {
-            if (tags.empty() || tag != tags.top()->first) {
-                s += "\": ";
-            }
-            
-            if(flag) {
-                tags.push(new mypair(tag, count));
-                flag = false;
-            }
-            if(!flag && tag != tags.top()->first) tags.push(new mypair(tag, count));
-
-            // CHECK IF DATA OR TAG CONTAINER
-            if(count > 1) {
-                s+= "[\n";
-                indentation_level++;
-                for(int j = 0; j < indentation_level; j++) s += "    ";
-            }
-
-        } else {
-            s += input_string[i];
-        }
+        if (input_string[i] == '>') s += "\": ";
+        else s += input_string[i];
         i = first_after_open_tag - 1;
 
 
