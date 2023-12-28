731e256d4c8da94d08418df9bb2184a3a7192916
SamehOssama <43967303+SamehOssama@users.noreply.github.com>
2023-12-28 15:01:48 +0200

    Revert "Bundled XML formatting code in a single function"
    This reverts commit 1cd8888aafe2ca45742d7ebfa30c3e09bb8153f8.

--- xml_formatter.cpp
+++ xml_formatter.cpp
@@ -17,60 +17,58 @@ void output_file (string path, string output_string) {
     out.close();
 }
 
-string prettify (string& xml_input) {
-    string prettified_xml = "<";
-    int i = xml_input.find('<') + 1, j;
-    int indentation_level = 0;
-    bool close_tag = false;
+int main() {
+    string input_string = read_file("sample.xml");
 
-    while(i < xml_input.length()) {
+    string s = "";
+    int i = input_string.find('<'), j;
+    int indentation_level = -1;
+    bool close_tag = false, file_start = true;
 
-        if (xml_input[i] == '<') {
-            int check_for_closing_tag = xml_input.find_first_not_of(" \n\r\t", i + 1);
+    while(i < input_string.length()) {
+
+        if (input_string[i] == '<') {
+            int check_for_closing_tag = input_string.find_first_not_of(" \n\r\t", i + 1);
             
-            if (xml_input[check_for_closing_tag] == '/') {
+            if (input_string[check_for_closing_tag] == '/') {
                 close_tag = true;
                 indentation_level--;
-            } else {
+            } else {    // it must be an opening tag
                 if (!close_tag) indentation_level++;
                 close_tag = false;
             }
 
-            prettified_xml += "\n";
-            for(int j = 0; j < indentation_level; j++) prettified_xml += "    ";
+            if(!file_start) s += "\n";
+            for(int j = 0; j < indentation_level; j++) s += "    ";
+            file_start = false;
         }
 
-        int first_after_open_tag = xml_input.find_first_not_of(" \n\r\t", i + 1);
+        int first_after_open_tag = input_string.find_first_not_of(" \n\r\t", i + 1);
         if (first_after_open_tag == -1) {
-            prettified_xml += '>';
+            s += '>';
             break;
         }
 
-        if (xml_input[i] == '>' && xml_input[first_after_open_tag] != '<') {
+        if (input_string[i] == '>' && input_string[first_after_open_tag] != '<') {
             indentation_level++;
 
-            prettified_xml += ">\n";
-            for(int j = 0; j < indentation_level; j++) prettified_xml += "    ";
+            s += ">\n";
+            for(int j = 0; j < indentation_level; j++) s += "    ";
 
-            int closing_tag_start_index = xml_input.find_first_of('<', i + 1);
-            int data_end_index = xml_input.find_last_not_of(" \n\r\t", closing_tag_start_index - 1);
-            prettified_xml += xml_input.substr(first_after_open_tag, data_end_index - first_after_open_tag + 1);
+            int closing_tag_start_index = input_string.find_first_of('<', i + 1);
+            int data_end_index = input_string.find_last_not_of(" \n\r\t", closing_tag_start_index - 1);
+            s += input_string.substr(first_after_open_tag, data_end_index - first_after_open_tag + 1);
             i = closing_tag_start_index;
             continue;
         }
         
-        prettified_xml += xml_input[i];
+        s += input_string[i];
         i = first_after_open_tag - 1;
+
         i++;
     }
 
-    return prettified_xml;
-}
-
-int main() {
-    string input_string = read_file("sample.xml");
-    string output = prettify(input_string);
-    output_file("output_pretty.xml", output);
+    output_file("output_pretty.xml", s);
 
     return 0;
 }
