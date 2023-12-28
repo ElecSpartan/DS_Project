5554f02274f37d827b51d90346ecc412b141a5a6
MazenSaaed12 <80298280+MazenSaaed11@users.noreply.github.com>
2023-12-28 21:06:24 +0200

    "Added compression code"

--- main.cpp
+++ main.cpp
@@ -1,9 +1,6 @@
 #include "Error_detection_and_correction.h"
-#include "huffman.h"
 int main() {
 
-    string file = read_file("E:\\College\\Data structures\\Project\\DS_Project\\sample.txt");
-    compress(file,"E:\\College\\Data structures\\Project\\DS_Project\\kk.bin");
-    cout<<decompress("E:\\College\\Data structures\\Project\\DS_Project\\kk.bin");
+
     return 0;
 }
