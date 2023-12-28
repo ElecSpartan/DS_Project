f8670ddf9e3c446b4527da2a4280745885b6f1b4
AmgadShrief <100617535+AmgadShrief@users.noreply.github.com>
2023-12-28 23:04:26 +0200

    Update huffman.h

--- huffman.h
+++ huffman.h
@@ -6,24 +6,24 @@
 #include <vector>
 #include <queue>
 
-struct Node {
+struct HuffmanNode {
     int data;
     int frequency;
-    Node* left;
-    Node* right;
+    HuffmanNode* left;
+    HuffmanNode* right;
 
-    Node(int data, int freq);
+    HuffmanNode(int data, int freq);
 };
 
 struct CompareNodes {
-    bool operator()(const Node* a, Node* b) const;
+    bool operator()(const HuffmanNode* a, HuffmanNode* b) const;
 };
 
-using HuffmanPriorityQueue = priority_queue<Node*, vector<Node*>, CompareNodes>; //min heap to put the min two char at the top of pq
+using HuffmanPriorityQueue = priority_queue<HuffmanNode*, vector<HuffmanNode*>, CompareNodes>; //min heap to put the min two char at the top of pq
 
-Node* buildHuffmanTree(const unordered_map<char, int>& frequencies); //to output the compressed file from the tree
-void HuffmanTree_to_file(Node* root, std::ofstream& outFile); //to build the tree from the compressed file
-Node* file_to_HuffmanTree(std::ifstream& inFile);
-void buildHuffmanCodes(Node* root, const string& code, unordered_map<char, string>& codes);
+HuffmanNode* buildHuffmanTree(const unordered_map<char, int>& frequencies); //to output the compressed file from the tree
+void HuffmanTree_to_file(HuffmanNode* root, std::ofstream& outFile); //to build the tree from the compressed file
+HuffmanNode* file_to_HuffmanTree(std::ifstream& inFile);
+void HuffmanCodes(HuffmanNode* root, const string& code, unordered_map<char, string>& codes);
 
 #endif // HUFFMAN_H
