5d58d34c76655e88ba84a46a8138d244ef9ed216
MazenSaaed12 <80298280+MazenSaaed11@users.noreply.github.com>
2023-12-28 17:23:25 +0200

    "Added code for correction"

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include "Error_detection_and_correction.h"
+#include "huffman.h"
 int main() {
 
-    string file = read_file("E:\\College\\Data structures\\Project\\DS_Project\\sample.xml");
-    Graph g = parse(file);
-    visualize_graph(g);
+    string file = read_file("E:\\College\\Data structures\\Project\\DS_Project\\sample.txt");
+    compress(file,"E:\\College\\Data structures\\Project\\DS_Project","kk");
+    cout<<decompress("E:\\College\\Data structures\\Project\\DS_Project\\kk.bin");
     return 0;
 }
