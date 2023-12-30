74273d7cbe61a6b3486a16483fb4ee3b5cb68948
Omar Gamal <112751175+ElecSpartan@users.noreply.github.com>
2023-12-30 03:45:53 +0200

    final update

--- File.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <iostream>
-#include <fstream>
-
-class File {
-public:
-        static std::string readFile(std::string path);
-        static void outputFile(std::string path,std::string &s);
-};
--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include "Network_Analysis.h"
-#include "xmlParser.h"
-using namespace std;
-int main() {
-    string file = File::readFile("E:\\College\\Data structures\\Project\\DS_Project\\sample.txt");
-    Network_Analysis::graph_and_visualization(file);
-    cout<<Network_Analysis::post_search("economy");
-    return 0;
-}
